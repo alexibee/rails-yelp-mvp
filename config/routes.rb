1003964f59235a17ed195e9c91601c6638ae5372
Aleksejs Brehovs <aleksi.b.tr@gmail.com>
2022-02-16 15:24:59 +0000

    routes edit

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :restaurants, only: %i[index show new create] do
-    member do
-      resources :reviews, only: %i[new create]
-    end
+    resources :reviews, only: %i[new create]
   end
 end
