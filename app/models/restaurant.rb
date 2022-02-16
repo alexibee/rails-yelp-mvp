157a9499b39d72f0030a80832c461528905a12b9
Aleksejs Brehovs <aleksi.b.tr@gmail.com>
2022-02-16 12:21:54 +0000

    model validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
 class Restaurant < ApplicationRecord
+  CATEGORIES = %w[chinese italian japanese french belgian].freeze
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
+  validates :phone_number, format: { with: /\A(\+|\d)\d{11,14}/ }
+  has_many :reviews, dependent: :destroy
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 class Review < ApplicationRecord
   belongs_to :restaurant
+  validates :content, presense: true
+  validates :rating, inclusion: { in: (0..5) }
 end
